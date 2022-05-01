7a8a0054506b3a3c7aff9dbe49f1f3bc6b285971
unknown <Administrator@ISM-533.ismahm.local>
2022-05-01 10:01:42 +0530

    userdeleteoperation

--- src/app/doctorservice.service.ts
+++ src/app/doctorservice.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -10,7 +10,21 @@ export class DoctorserviceService {
   public addUser(data):Observable<any>{
     return this.http.post("https://primeflixapis.herokuapp.com/api/register", data);
   }
+  
  public getAllDoctors():Observable<any>{
-  return this.http.get("https://primeflixapis.herokuapp.com/api/getusers/https://primeflixapis.herokuapp.com/api/login/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNmQ0NWQwZjhkYmE5OTU0ZDljNzUxMyIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NTEzMjg0ODMsImV4cCI6MTY1MTU4NzY4M30.SGZcgBn_ejkzDciLD9N-CQsRxgNAWG3cKpwnLWdZdv8")
+  const httpOptions={
+    headers:new HttpHeaders({
+      'token':"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNmQ0NWQwZjhkYmE5OTU0ZDljNzUxMyIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NTEzMjg0ODMsImV4cCI6MTY1MTU4NzY4M30.SGZcgBn_ejkzDciLD9N-CQsRxgNAWG3cKpwnLWdZdv8"
+    })
+  }
+  return this.http.get("https://primeflixapis.herokuapp.com/api/getusers/",httpOptions)
+ }
+ public deleteDoctor(id):Observable <any>{
+   const httpOptions={
+    headers:new HttpHeaders({
+      'token':"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNmQ0NWQwZjhkYmE5OTU0ZDljNzUxMyIsImlzQWRtaW4iOmZhbHNlLCJpYXQiOjE2NTEzMjg0ODMsImV4cCI6MTY1MTU4NzY4M30.SGZcgBn_ejkzDciLD9N-CQsRxgNAWG3cKpwnLWdZdv8"
+    })}
+   return this.http.delete("https://primeflixapis.herokuapp.com/api/deleteUser/"+id,httpOptions);
+
  }
 }
